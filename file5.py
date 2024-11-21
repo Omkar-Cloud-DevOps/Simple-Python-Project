44f9a8ced23baebd6368390be3dc473a579a0f53
omkar <omkarjoshi2809@gmail.com>
2024-11-21 19:49:24 +0530

    another file

---
+++ file5.py
@@ -0,0 +1,16 @@
+def addition(num1,num2): 
+    add = num1 + num2 
+    return add 
+ 
+def subtract(num1,num2): 
+    sub = num2 - num1 
+    return sub 
+ 
+def multipication(num1,num2): 
+    mul = num1 * num2 
+    return mul 
+ 
+print(addition(50,10)) 
+print(subtract(5,10)) 
+print(multipication(10,20))
+
